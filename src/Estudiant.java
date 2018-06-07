2e95b449ba05d187cd55b4f8cd76df847ec4ffc7
Javier Viñas <javiervinasgarcia@gmail.com>
2018-06-07 19:42:29 +0200

    Ultimo COMMIT

---
+++ src/Estudiant.java
@@ -0,0 +1,12 @@
+public class Estudiant {
+    private String nom = "";
+    private String cognoms = "";
+    private String dni = "";
+
+    public Estudiant(){
+        this.nom = nom;
+        this.cognoms = cognoms;
+        this.dni = dni;
+    }
+
+}
--- src/Matricula.java
+++ src/Matricula.java
@@ -1,38 +1,34 @@
 import java.util.Collection;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Matricula {
 
+    public static final int CONSTANT_INT = 15;
     public long id = 0;
-    private String nom = "";
-    private String cognoms = "";
-    private String dni = "";
+
     private int curs = 0;
     private Collection assignatures;
 
-    public Matricula(){}
+    public Matricula() {
+    }
 
     public Matricula(long id, String nom, String cognoms, String dni, int curs, Collection assignatures) {
         this.id = id;
-        this.nom = nom;
-        this.cognoms = cognoms;
-        this.dni = dni;
         this.curs = curs;
         this.assignatures = assignatures;
     }
 
-    public float costMatricula(){
+    public float costMatricula() {
 
         float cost = 0;
         int credits = 0;
 
-        for (Iterator iter = assignatures.iterator(); iter.hasNext();) {
+        for (Iterator iter = assignatures.iterator(); iter.hasNext(); ) {
             Assignatura element = (Assignatura) iter.next();
             credits = credits + element.credits;
         }
 
-        cost = credits * 15;
+        cost = credits * CONSTANT_INT;
         return cost;
     }
 
